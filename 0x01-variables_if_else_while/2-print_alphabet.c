7abb806bbc770d24f4a028a2eb334c732f6260a6
HagerAlaa1 <hageralaamaher1@gmail.com>
2023-10-06 13:08:08 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *Return: 0
+ */
+int main(void)
+{
+	char Alphabet;
+
+	for (Alphabet = 'A'; Alphabet <= 'Z'; Alphabet++)
+
+		putchar(Alphabet);
+
+	return (0);
+}
